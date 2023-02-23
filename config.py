7588aa8fd0d57174fd4c80f487a75dfe28e0a421
xdgirl <106166192+xdgirl@users.noreply.github.com>
2023-02-23 13:25:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,12 +21,12 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "1356469075").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AnonymousX1025/AnonXMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "@ll_noT_uR_tanu_ll")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/DevilsHeavenMF")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "@ll_noT_uR_tanu_ll")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "@ll_noT_uR_tanu_ll")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
