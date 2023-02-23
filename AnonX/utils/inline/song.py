8528e90cda7fad31fe8f5291e6b172d0740db99a
xdgirl <106166192+xdgirl@users.noreply.github.com>
2023-02-23 13:15:18 +0530

    Update song.py

--- AnonX/utils/inline/song.py
+++ AnonX/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"{config.SUPPORT_GROUP}",
+                text="🥀 sᴜᴩᴩᴏʀᴛ[ᴛᴀɴᴜ🥀] 🥀", url=f"{config.SUPPORT_GROUP}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
