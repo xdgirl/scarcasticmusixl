842d5f7639fdcf98b86776694c47249d304fbcd7
xdgirl <106166192+xdgirl@users.noreply.github.com>
2023-09-11 16:47:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,12 +16,12 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "ᴀɴᴏɴ ダ ᴍᴜsɪᴄ")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1356469075").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5783480961").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://t.me/ll_noT_uR_tanu_ll")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://t.me/ll_tanu_ll")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
